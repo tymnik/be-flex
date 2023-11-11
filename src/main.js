fa157e2af8d53d065c5fddf29c306292353ccde3
Veronika Timakova <tymnik@ukr.net>
2023-11-12 02:43:52 +0300

    basics

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './partials/header/header';
+import './partials/hero/hero';
+import './partials/about-me/about-me';
+import './partials/you-will-find/you-will-find';
+import './partials/for-whom/for-whom';
+import './partials/apparatus/apparatus';
+import './partials/about-training/about-training';
+import './partials/what-to-take/what-to-take';
+import './partials/submit/submit';
+import './partials/answer-questions/answer-questions';
+import './partials/message-me/message-me';
+import './partials/gallery/gallery';
+import './partials/reviews/reviews';
+import './partials/contact-me/contact-me';
+import './partials/follow-me/follow-me';
+import './partials/footer/footer';
+
