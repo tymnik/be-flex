9f377b57f2b01161437722c8175317374556844e
Veronika Timakova <tymnik@ukr.net>
2023-11-14 03:06:31 +0300

    feat: about-me

--- src/partials/about-me/about-me.js
+++ src/partials/about-me/about-me.js
@@ -1,24 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const introText = document.querySelector('.intro-text');
-  const ellipsis = document.createElement('span');
-  ellipsis.textContent = '...';
-  ellipsis.classList.add('ellipsis');
-  introText.appendChild(ellipsis);
-
-  const showMoreBtn = document.createElement('button');
-  showMoreBtn.textContent = 'read more';
-  showMoreBtn.classList.add('show-more-btn');
-  introText.appendChild(showMoreBtn);
-
-  const showLessBtn = document.createElement('button');
-  showLessBtn.textContent = 'read less';
-  showLessBtn.classList.add('show-less-btn');
-  introText.appendChild(showLessBtn);
+  const showMoreBtn = document.querySelector('.show-more-btn');
+  const showLessBtn = document.querySelector('.show-less-btn');
+  const ellipsis = document.querySelector('.ellipsis');
 
   showMoreBtn.addEventListener('click', function () {
     introText.classList.add('collapsed');
     showMoreBtn.style.display = 'none';
     showLessBtn.style.display = 'inline';
+
+    if (ellipsis) {
+      introText.removeChild(ellipsis);
+    }
   });
 
   showLessBtn.addEventListener('click', function () {
