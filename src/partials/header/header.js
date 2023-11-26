81f33bea62dbc1e2910e0e78516c97adf2af2eb9
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-11-26 22:09:38 +0200

    added tablet + pc

--- src/partials/header/header.js
+++ src/partials/header/header.js
@@ -1,12 +1,23 @@
 const mobileMenuOpen = document.querySelector('.header-burger-opener');
 const mobileMenuClose = document.querySelector('.header-burger-closer');
+const mobileMenu = document.querySelector('.mobile-menu-container');
 
 mobileMenuOpen.addEventListener('click', toggleMobileMenu);
 mobileMenuClose.addEventListener('click', toggleMobileMenu);
 
+addEventListener('click', e => {
+  if (mobileMenu.style.display !== 'none') {
+    mobileMenu.style.display = 'none';
+    mobileMenuOpen.style.visibility = 'visible';
+  }
+});
+
 function toggleMobileMenu() {
-  const mobileMenu = document.querySelector('.mobile-menu-container');
   const currentDisplayStyle = window.getComputedStyle(mobileMenu).display;
 
+  const openerVisibility = window.getComputedStyle(mobileMenuOpen).visibility;
+
   mobileMenu.style.display = currentDisplayStyle === 'none' ? 'block' : 'none';
+  mobileMenuOpen.style.visibility =
+    openerVisibility === 'visible' ? 'hidden' : 'visible';
 }
