00cff46be23b93c4f7c401abbdd7e4e57962af4c
LadunovaNadia <ladunova.nadia@gmail.com>
2023-11-27 13:44:37 +0200

    update

--- src/partials/submit/submit.js
+++ src/partials/submit/submit.js
@@ -4,13 +4,15 @@ import 'intl-tel-input/build/css/intlTelInput.css';
 document.addEventListener('DOMContentLoaded', function () {
     var input = document.querySelector("#user-tel");
     var iti = intlTelInput(input, {
-    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
         preferredCountries: ["ae", "us", "gb", "ca", "au", "ua"],
         separateDialCode: true,
     });
 
     input.addEventListener("click", function () {
-        iti.openDropdown();
+        if (typeof iti.openDropdown === 'function') {
+            iti.openDropdown();
+        }
     });
 
     input.addEventListener("countrychange", function (event) {
@@ -19,3 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Заборона вводу літер
+document.getElementById('user-tel').addEventListener('input', function (event) {
+    this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+// Приховання стрілки після вибору коду країни
+const userTelInput = document.getElementById('user-tel');
+
+userTelInput.addEventListener('countrychange', function () {
+    
+    setTimeout(function () {
+        var arrowElement = document.querySelector('.iti__arrow');
+        arrowElement.style.display = 'none';
+    }, 100);
+});
