899c3e7caa0c8987bd822a1b68cdce7bb2c07c0d
Veronika Timakova <tymnik@ukr.net>
2023-11-27 18:02:31 +0300

    bugfix: submit

--- src/partials/submit/submit.js
+++ src/partials/submit/submit.js
@@ -2,48 +2,35 @@ import intlTelInput from 'intl-tel-input';
 import 'intl-tel-input/build/css/intlTelInput.css';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var input = document.querySelector("#user-tel");
-    var iti = intlTelInput(input, {
-        // utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
-        utilsScript: "./node_modules/intl-tel-input/build/js/utils.js",
-        preferredCountries: ["ae", "us", "gb", "ca", "au", "ua"],
-        separateDialCode: true,
-    });
+  var input = document.querySelector('#user-tel');
+  var iti = intlTelInput(input, {
+    utilsScript:
+      'https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js',
+    preferredCountries: ['ae', 'us', 'gb', 'ca', 'au', 'ua'],
+    separateDialCode: true,
+  });
 
-    input.style.backgroundImage = 'url("/node_modules/intl-tel-input/build/img/flags.png")';
+  input.addEventListener('click', function () {
+    if (typeof iti.openDropdown === 'function') {
+      iti.openDropdown();
+    }
+  });
 
-    input.addEventListener("click", function () {
-        if (typeof iti.openDropdown === 'function') {
-            iti.openDropdown();
-        }
-    });
-
-    input.addEventListener("countrychange", function (event) {
-        var selectedCountry = iti.getSelectedCountryData();
-        console.log("Selected country code: " + selectedCountry.dialCode);
-
-        if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia('(min-resolution: 192dpi)').matches)) {
-            input.style.backgroundImage = 'url("/node_modules/intl-tel-input/build/img/flags@2x.png")';
-        }
-    });
-
-    input.addEventListener('countrychange', function () {
-        setTimeout(function () {
-            var arrowElement = document.querySelector('.iti__arrow');
-            arrowElement.style.display = 'none';
-        }, 100);
-    });
+  input.addEventListener('countrychange', function (event) {
+    var selectedCountry = iti.getSelectedCountryData();
+    console.log('Selected country code: ' + selectedCountry.dialCode);
+  });
 });
 
 document.getElementById('user-tel').addEventListener('input', function (event) {
-    this.value = this.value.replace(/[^0-9]/g, '');
+  this.value = this.value.replace(/[^0-9]/g, '');
 });
 
 const userTelInput = document.getElementById('user-tel');
 
 userTelInput.addEventListener('countrychange', function () {
-    setTimeout(function () {
-        var arrowElement = document.querySelector('.iti__arrow');
-        arrowElement.style.display = 'none';
-    }, 100);
+  setTimeout(function () {
+    var arrowElement = document.querySelector('.iti__arrow');
+    arrowElement.style.display = 'none';
+  }, 100);
 });
