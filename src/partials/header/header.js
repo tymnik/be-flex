6d43184f33c7d5100b8e1697023a23fef447a66a
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-12-04 23:00:47 +0200

    Further work

--- src/partials/header/header.js
+++ src/partials/header/header.js
@@ -3,6 +3,8 @@ const mobileMenuClose = document.querySelector('.header-burger-closer');
 const mobileMenu = document.querySelector('.mobile-backdrop');
 const mobileMenuIcon = document.querySelector('.header-burger-icon');
 const headerNav = document.querySelectorAll('.mobile-nav-link');
+const desktopPhoneIcon = document.querySelector('.header-phone-icon');
+const desktopPhoneNumber = document.querySelector('.header-phone-number');
 const body = document.querySelector('body');
 
 headerNav.forEach(link => link.addEventListener('click', close));
@@ -15,14 +17,40 @@ mobileMenu.addEventListener('click', e => {
   }
 });
 
-function open(e) {
+function open() {
   mobileMenu.style.display = 'block';
   mobileMenuOpen.style.visibility = 'hidden';
   body.style.overflow = 'hidden';
 }
 
-function close(e) {
+function close() {
   mobileMenu.style.display = 'none';
   mobileMenuOpen.style.visibility = 'visible';
   body.style.overflow = 'visible';
+}
+
+desktopPhoneIcon.addEventListener('click', function () {
+  const phoneNumber = '+971524116236';
+  copyPhoneNumber(phoneNumber);
+});
+
+desktopPhoneNumber.addEventListener('click', function () {
+  const phoneNumber = '+971524116236';
+  copyPhoneNumber(phoneNumber);
+});
+
+function copyPhoneNumber(phoneNumber) {
+  const tempTextArea = document.createElement('textarea');
+  tempTextArea.value = phoneNumber;
+
+  document.body.appendChild(tempTextArea);
+
+  tempTextArea.select();
+  tempTextArea.setSelectionRange(0, 99999); /* For mobile devices */
+
+  document.execCommand('copy');
+
+  document.body.removeChild(tempTextArea);
+
+  alert('Phone number copied to clipboard: ' + phoneNumber);
 }
