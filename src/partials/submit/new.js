9c968faa682ba0c0f03a191f43f6fe244ec0bb7d
LadunovaNadia <ladunova.nadia@gmail.com>
2023-12-08 22:36:57 +0200

    update_form

---
+++ src/partials/submit/new.js
@@ -0,0 +1,130 @@
+import intlTelInput from 'intl-tel-input';
+import 'intl-tel-input/build/css/intlTelInput.css';
+import axios from 'axios';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const input = document.querySelector('#user-tel');
+    const iti = intlTelInput(input, {
+        utilsScript: 'https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js',
+        preferredCountries: ['ae', 'us', 'gb', 'ca', 'au', 'ua'],
+        separateDialCode: true,
+    });
+
+    input.addEventListener('click', function () {
+        if (typeof iti.openDropdown === 'function') {
+            iti.openDropdown();
+        }
+    });
+
+    input.addEventListener('countrychange', function (event) {
+        var selectedCountry = iti.getSelectedCountryData();
+        console.log('Selected country code: ' + selectedCountry.dialCode);
+    });
+
+    document.getElementById('user-tel').addEventListener('input', function (event) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+
+    const userTelInput = document.getElementById('user-tel');
+
+    userTelInput.addEventListener('countrychange', function () {
+        setTimeout(function () {
+            var arrowElement = document.querySelector('.iti__arrow');
+            arrowElement.style.display = 'none';
+        }, 100);
+    });
+    // ------------------------------------------------------------------------------
+    
+    document.getElementById('btn').addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var formElements = document.querySelectorAll('.sign_up_form_input, .modal-check');
+
+        formElements.forEach(function (element) {
+            if (element.value.trim() === '') {
+                element.classList.add('red-border');
+            } else {
+                element.classList.remove('red-border');
+            }
+        });
+
+        const allFieldsEmpty = Array.from(formElements).every(function (element) {
+            return element.value.trim() === '';
+        });
+
+        if (allFieldsEmpty || document.getElementById('user-tel').value.trim() === '') {
+            document.getElementById('user-tel').classList.add('red-border');
+            console.log('Не можна відправити порожню форму');
+            return; 
+        }
+
+        const formData = new FormData(document.querySelector('.sign_up_form'));
+        const telegramToken = '6943310494:AAF9IRuXS0o6ejGqdXoa_ZHePm6PcEwLLLA';
+        const ChatId = '-1002131222060';
+        const Url_api = `https://api.telegram.org/bot${telegramToken}`;
+
+        let message = 'Нова заявка:\n\n';
+        message += `<b>Відправник: </b> ${document.getElementById('user-name').value}\n`;
+        message += `<b>Телефон: </b> +${iti.getSelectedCountryData().dialCode} ${document.getElementById('user-tel').value}\n`;
+        message += `<b>Повідомлення: </b> ${document.getElementById('user-comment').value}\n`;
+
+        formData.forEach(function (value, key) {
+            message += key + ': ' + value + '\n';
+        });
+
+        axios.post(`${Url_api}/sendMessage`, {
+            chat_id: ChatId,
+            parse_mode: 'html',
+            text: message,
+        }).then(function (response) {
+            console.log('Повідомлення відправлено до Telegram:', response.data);
+        }).catch(function (error) {
+            console.error('Помилка відправлення повідомлення до Telegram:', error);
+        });
+    });
+});
+
+
+
+
+// -------------------------
+
+const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.getAttribute("type") === 'checkbox' && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+    
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
--- src/partials/submit/submit.js
+++ src/partials/submit/submit.js
@@ -17,111 +17,101 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     input.addEventListener('countrychange', function (event) {
-        const selectedCountry = iti.getSelectedCountryData();
+        var selectedCountry = iti.getSelectedCountryData();
         console.log('Selected country code: ' + selectedCountry.dialCode);
-        const arrowElement = document.querySelector('.iti__arrow');
-        arrowElement.style.display = 'none';
     });
-
-    const selectedDialCodeElement = document.querySelector('.iti__selected-dial-code');
-    selectedDialCodeElement.classList.add('vfkb');
 
     document.getElementById('user-tel').addEventListener('input', function (event) {
         this.value = this.value.replace(/[^0-9]/g, '');
-        validatePhoneNumber();
-    });
-
-    const userNameInput = document.getElementById('user-name');
-    userNameInput.addEventListener('input', function (event) {
-        validateNameInput();
     });
 
     const userTelInput = document.getElementById('user-tel');
-    userTelInput.addEventListener('change', function () {
+
+    userTelInput.addEventListener('countrychange', function () {
         setTimeout(function () {
             var arrowElement = document.querySelector('.iti__arrow');
             arrowElement.style.display = 'none';
         }, 100);
-        validatePhoneNumber();
     });
-
-    // -----------------------
-    
-    document.getElementById('btn').addEventListener('click', function (event) {
-        event.preventDefault();
-
-        const formElements = document.querySelectorAll('.modal-check');
-        const privacyCheckbox = document.getElementById('user-privacy');
-
-        validatePhoneNumber();
-        validateNameInput();
-
-        
-        
-        // privacyCheckbox.addEventListener('change', function () {
-        //     if (privacyCheckbox.checked) {
-        //         privacyCheckbox.classList.remove('red-border');
-        //         privacyCheckbox.style.border = '1px solid var(--main-2, #6B39D0)';
-        //     } else {
-        //         privacyCheckbox.classList.add('red-border');
-        //         privacyCheckbox.style.border = '2px solid var(--error, #A61717)';
-        //     }
-        // });
-
-    // const allFieldsEmpty = Array.from(formElements).every(function (element) {
-    //     return element.value.trim() === '' || element === document.getElementById('user-comment');
-    // });
-
-    // if (allFieldsEmpty || userTelInput.classList.contains('red-border') || userNameInput.classList.contains('red-border') || !privacyCheckbox.checked) {
-    //     console.log('Не можна відправити порожню форму або з невалідним номером телефону, ім\'ям або без погодження з політикою конфіденційності');
-    //     return;
-    // }
+});
 
 
 
 
-    // -----------------------
+// ------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = 0;
+
+        const formElements = document.querySelectorAll('.submit_form_input, .sign_up_checkbox_input');
+
+        formElements.forEach((input) => {
+            removeError(input);
+
+            if (input.type === 'checkbox' && !input.checked) {
+                addError(input);
+                error++;
+            } else if (input.id === 'user-tel') {
+                const telInput = document.getElementById('user-tel');
+                if (telInput.value.trim() === '') {
+                    addError(telInput);
+                    error++;
+                } else {
+                    removeError(telInput);
+                }
+            } else if (input.value.trim() === '') {
+                addError(input);
+                error++;
+            }
+        });
+
+        if (error > 0) {
+            console.log('Не можна відправити форму з помилками валідації');
+            return;
+        }
+
+        // Отримання даних і відправлення форми, якщо валідація успішна
+        try {
         const formData = new FormData(document.querySelector('.sign_up_form'));
         const telegramToken = '6943310494:AAF9IRuXS0o6ejGqdXoa_ZHePm6PcEwLLLA';
         const ChatId = '-1002131222060';
         const Url_api = `https://api.telegram.org/bot${telegramToken}`;
 
-        let message = 'Нова заявка:\n\n';
-        message += `<b>Відправник: </b> ${userNameInput.value}\n`;
-        message += `<b>Телефон: </b> +${iti.getSelectedCountryData().dialCode} ${userTelInput.value}\n`;
-        message += `<b>Повідомлення: </b> ${document.getElementById('user-comment').value}\n`;
+            let message = 'Нова заявка:\n\n';
+            message += `<b>Відправник: </b> ${document.getElementById('user-name').value}\n`;
+            message += `<b>Телефон: </b> +${iti.getSelectedCountryData().dialCode} ${document.getElementById('user-tel').value}\n`;
+            message += `<b>Повідомлення: </b> ${document.getElementById('user-comment').value}\n`;
 
-        formData.forEach(function (value, key) {
-        message += key + ': ' + value + '\n';
-        });
+            formData.forEach(function (value, key) {
+                message += key + ': ' + value + '\n';
+            });
 
-        axios.post(`${Url_api}/sendMessage`, {
-        chat_id: ChatId,
-        parse_mode: 'html',
-        text: message,
-        }).then(function (response) {
-        console.log('Повідомлення відправлено до Telegram:', response.data);
-        }).catch(function (error) {
-        console.error('Помилка відправлення повідомлення до Telegram:', error);
-        });
-    });
+            const response = await axios.post(`${Url_api}/sendMessage`, {
+                chat_id: ChatId,
+                parse_mode: 'html',
+                text: message,
+            });
 
-    function validatePhoneNumber() {
-        if (userTelInput.value.trim() === '' || !iti.isValidNumber()) {
-        userTelInput.classList.add('red-border');
-        } else {
-        userTelInput.classList.remove('red-border');
+            console.log('Повідомлення відправлено до Telegram:', response.data);
+        } catch (error) {
+            console.error('Помилка відправлення повідомлення до Telegram:', error);
         }
     }
 
-    function validateNameInput() {
-        if (userNameInput.value.trim().length === 1) {
-        userNameInput.classList.add('red-border');
-        } else {
-        userNameInput.classList.remove('red-border');
-        }
-
-        
+    function addError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
     }
-    
+
+    function removeError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
 });
+
+
