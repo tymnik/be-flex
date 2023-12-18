f7757a015978516ce4a283d9f52b5074bef1b7d6
Veronika Timakova <tymnik@ukr.net>
2023-12-18 13:19:20 +0300

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
@@ -8,12 +7,15 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/be-flex/',
     root: 'src',
     build: {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: {
+          main: './src/main.js',
+        },
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
